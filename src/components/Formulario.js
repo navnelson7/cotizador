95159a2a40ff71cd2cf820565b8e51c9bc60d024
navnelson7 <navnelson7@gmailcom>
2020-04-07 23:32:47 -0600

    Finalizando el styled component del Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,19 +1,61 @@
 import React from 'react';
+import styled from '@emotion/styled';
+//creando styled components
+const Campo = styled.div`
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center; 
+`;
+
+const Label = styled.label`
+    flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+    display:block;
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+    margin:0 1rem;
+`;
+
+const Button = styled.button`
+    background-color: #00838f;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #ffffff;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 2rem;
+
+    &:hover{
+        cursor:pointer;
+        background-color: #26c6da;
+    }
+`;
+
 const Formulario = () => {
     return ( 
         <form>
-            <div>
-                <label>Marca: </label>
-                <select>
+            <Campo>
+                <Label>Marca: </Label>
+                <Select>
                     <option value=''>----- Selecciones -----</option>
                     <option value='americano'>Americano-</option>
                     <option value='europeo'>Europeo</option>
                     <option value='asiatico'>Asiatico</option>
-                </select>
-            </div>
-            <div>
-                <label>Año: </label>
-                    <select>
+                </Select>
+            </Campo>
+            <Campo>
+                <Label>Año: </Label>
+                    <Select>
                         <option value="">-- Seleccione --</option>
                         <option value="2021">2021</option>
                         <option value="2020">2020</option>
@@ -25,25 +67,25 @@ const Formulario = () => {
                         <option value="2014">2014</option>
                         <option value="2013">2013</option>
                         <option value="2012">2012</option>
-                    </select>
-            </div>
-            <div>
-                <label>Plan: </label>
-                <input 
+                    </Select>
+            </Campo>
+            <Campo>
+                <Label>Plan: </Label>
+                <InputRadio 
                     type='radio'
                     name='plan'
                     value='basico'
                 /> Básico
 
-                <input 
+                <InputRadio 
                     type='radio'
                     name='plan'
                     value='completo'
                 /> Completo
-            </div>
-            <button
+            </Campo>
+            <Button
                 type="button"
-            >Cotizar</button>
+            >Cotizar</Button>
         </form>
      );
 }
